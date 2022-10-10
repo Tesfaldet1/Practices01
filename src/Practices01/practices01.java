976f893eed800ed2251e5b355823a94819225783
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2022-10-10 13:36:37 +0200

    Initial commit

---
+++ src/Practices01/practices01.java
@@ -0,0 +1,18 @@
+package Practices01;
+
+public class practices01 {
+    public static void main(String[] args) {
+        String _firstname = "Tesfaldet";
+        String _lastName= "Weldemiche";
+        float _number1 = 23.89f;
+        double _number2 = 20.1;
+        double result = _number1 + _number2;
+
+        System.out.println(_firstname);
+        System.out.println(_lastName);
+
+        System.out.println("FullName  " + _firstname + " "+ _lastName);
+        System.out.println("The sum of the numbers is " + result);
+    }
+
+}
